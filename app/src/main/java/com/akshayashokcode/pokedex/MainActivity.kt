606d4119f3df89aa2ed9908c438eef6d52d7be19
Riderpool1 <akshayashokan1054.com>
2021-10-12 19:11:23 +0530

    Initial commit

---
+++ app/src/main/java/com/akshayashokcode/pokedex/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.akshayashokcode.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+import com.akshayashokcode.pokedex.ui.theme.PokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokedexTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController,
+                startDestination = "pokemon_list_screen"){
+                    composable("pokemon_list_screen"){
+
+                    }
+                    composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                    arguments = listOf(
+                        navArgument("dominantColor"){
+                            type= NavType.IntType
+                        },
+                        navArgument("pokemonName"){
+                            type= NavType.StringType
+                        }
+                    )){
+                        val dominantColor= remember {
+                            val color=it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) }?: Color.White
+                        }
+                        val pokemonName=remember{
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PokedexTheme {
+        Greeting("Android")
+    }
+}
